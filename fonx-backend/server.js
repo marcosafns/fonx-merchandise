66e0cfa5ff1aba61f74be875b5f5695840440fff
Marcos Afonso <marcos.afonsoo@outlook.com>
2025-04-29 15:04:47 -0300

    commit server.js 3

--- fonx-backend/server.js
+++ fonx-backend/server.js
@@ -19,13 +19,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://fonx.com.br');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
 
 // Rotas
 app.get('/api/hello', (req, res) => {
