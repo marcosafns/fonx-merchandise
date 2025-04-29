81bdf6fee9e83170027ecb48883866aa089ba84d
Marcos Afonso <marcos.afonsoo@outlook.com>
2025-04-29 16:24:04 -0300

    Update page.tsx

--- fonx-frontend/src/app/login/page.tsx
+++ fonx-frontend/src/app/login/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import Cookies from 'js-cookie';
 import '@/styles/styles.css';
 import BackToHome from '../components/BackToHome';
 import Toast from '../components/Toast'; // importa o Toast
